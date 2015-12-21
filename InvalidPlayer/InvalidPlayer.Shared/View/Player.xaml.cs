1c8a8a46958266d46f91a8d542e20db6d03a9c4f
saki <saki@saki.saki>
2015-12-21 11:46:32 +0800

    格式化代码

--- InvalidPlayer/InvalidPlayer.Shared/View/Player.xaml.cs
+++ InvalidPlayer/InvalidPlayer.Shared/View/Player.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Storage.Streams;
-using Windows.Storage.Pickers;
-using Windows.UI.Popups;
 using InvalidPlayer.Parser;
 using InvalidPlayer.Parser.Youku;
 
@@ -23,22 +11,22 @@ namespace InvalidPlayer.View
 {
     public sealed partial class Player : Page
     {
+        private readonly IVideoUrlParser _youkuVideoUrlParser;
+
         public Player()
         {
-            this.InitializeComponent();
+            InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Required;
             _youkuVideoUrlParser = new YoukuParser();
         }
-        private IVideoUrlParser _youkuVideoUrlParser;
 
         private void player_BufferingProgressChanged(object sender, RoutedEventArgs e)
         {
-       
         }
 
         private void player_DownloadProgressChanged(object sender, RoutedEventArgs e)
         {
-            BufferingProgressTextBlock.Text = $"Downloaded {MainPlayer.DownloadProgress * 100}%";
+            BufferingProgressTextBlock.Text = $"Downloaded {MainPlayer.DownloadProgress*100}%";
         }
 
         private async void YoukuBtn_OnClick(object sender, RoutedEventArgs e)
@@ -62,17 +50,14 @@ namespace InvalidPlayer.View
                     {
                         plist.Append(url, 0, 0);
                     }
-                    var s = "plist://WinRT-TemporaryFolder_" + System.IO.Path.GetFileName(await plist.SaveAndGetFileUriAsync());
+                    var s = "plist://WinRT-TemporaryFolder_" + Path.GetFileName(await plist.SaveAndGetFileUriAsync());
                     MainPlayer.Source = new Uri(s);
-                  
                 }
                 catch (Exception exception)
                 {
-                   var t= new MessageDialog(exception.Message).ShowAsync();
+                    var t = new MessageDialog(exception.Message).ShowAsync();
                 }
             }
         }
-
-    
     }
 }
