415abe46f1ad5f97258f6240f8148a74a4151ec1
saki <saki@saki.saki>
2015-12-23 19:31:49 +0800

    add protocol

--- InvalidPlayer/InvalidPlayer.Shared/View/Player.xaml.cs
+++ InvalidPlayer/InvalidPlayer.Shared/View/Player.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -19,11 +21,31 @@ namespace InvalidPlayer.View
         {
             InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Required;
-            _youkuVideoUrlParser = new YoukuParser();
+            _videoParser = new VideoParser();
+        }
+
+        protected  override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //weburl://?url=http://v.youku.com/v_show/id_XMTM1MTUzOTY1Ng==.html
+            if (e.Parameter is Uri)
+            {
+                var uri = (Uri) e.Parameter;
+                var query = uri.Query;
+                if (!string.IsNullOrEmpty(query))
+                {
+                    var form = new WwwFormUrlDecoder(query);
+                    var url = form.GetFirstValueByName("url");
+                    if (!string.IsNullOrEmpty(url))
+                    {
+                        await Play(url);
+                    }
+                }
+            }
         }
 
         private void player_BufferingProgressChanged(object sender, RoutedEventArgs e)
         {
+            BufferingProgressTextBlock.Text = $"{MainPlayer.BufferingProgress* 100}%";
         }
 
         private void player_DownloadProgressChanged(object sender, RoutedEventArgs e)
@@ -33,44 +55,49 @@ namespace InvalidPlayer.View
 
         private async void YoukuBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            var youkuUrl = WebUrlTextBox.Text;
-            if (!string.IsNullOrEmpty(youkuUrl))
+            var url = WebUrlTextBox.Text;
+            if (!string.IsNullOrEmpty(url))
             {
-                if (MainPlayer.CurrentState == MediaElementState.Playing)
+                await Play(url);
+            }
+        }
+
+        private async Task Play(string url)
+        {
+            if (MainPlayer.CurrentState == MediaElementState.Playing)
+            {
+                MainPlayer.Stop();
+            }
+
+            try
+            {
+                var videos = await _videoParser.ParseAsync(url);
+                if (videos.Count > 1)
                 {
-                    MainPlayer.Stop();
-                }
-                try
-                {
-                    var videos = await _youkuVideoUrlParser.ParseAsync(youkuUrl);
-                    if (videos.Count > 1)
+                    var plist = new Playlist(PlaylistTypes.NetworkHttp);
+                    var cfgs = default(PlaylistNetworkConfigs);
+                    cfgs.DownloadRetryOnFail = true;
+                    cfgs.DetectDurationForParts = false;
+                    cfgs.HttpUserAgent = string.Empty;
+                    cfgs.HttpReferer = string.Empty;
+                    cfgs.HttpCookie = string.Empty;
+                    cfgs.TempFilePath = string.Empty;
+                    plist.NetworkConfigs = cfgs;
+                    foreach (var video in videos)
                     {
-                        var plist = new Playlist(PlaylistTypes.NetworkHttp);
-                        var cfgs = default(PlaylistNetworkConfigs);
-                        cfgs.DownloadRetryOnFail = true;
-                        cfgs.DetectDurationForParts = false;
-                        cfgs.HttpUserAgent = string.Empty;
-                        cfgs.HttpReferer = string.Empty;
-                        cfgs.HttpCookie = string.Empty;
-                        cfgs.TempFilePath = string.Empty;
-                        plist.NetworkConfigs = cfgs;
-                        foreach (var video in videos)
-                        {
-                            plist.Append(video.Url, video.Size, video.Seconds);
-                        }
-                        var s = "plist://WinRT-TemporaryFolder_" +
-                                Path.GetFileName(await plist.SaveAndGetFileUriAsync());
-                        MainPlayer.Source = new Uri(s);
+                        plist.Append(video.Url, video.Size, (float) video.Seconds);
                     }
-                    else if (videos.Count == 1)
-                    {
-                        MainPlayer.Source = new Uri(videos[0].Url);
-                    }
+                    var s = "plist://WinRT-TemporaryFolder_" + Path.GetFileName(await plist.SaveAndGetFileUriAsync());
+                    MainPlayer.Source = new Uri(s);
                 }
-                catch (Exception exception)
+                else if (videos.Count == 1)
                 {
-                    var t = new MessageDialog(exception.Message).ShowAsync();
+                    MainPlayer.Source = new Uri(videos[0].Url);
                 }
+            }
+            catch (Exception exception)
+            {
+                var t = new MessageDialog(exception.Message).ShowAsync();
             }
         }
     }
