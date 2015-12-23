1235ee49f0292f18c3e82482c66b886e30b13d5b
saki <saki@saki.saki>
2015-12-23 19:32:12 +0800

    add firefox plugin

---
+++ WebPlugin/firefox/index.js
@@ -0,0 +1,36 @@
+var self = require('sdk/self');
+
+// a dummy function, to show how tests work.
+// to see how to test this function, look at test/test-index.js
+function dummy(text, callback) {
+  callback(text);
+}
+
+exports.dummy = dummy;
+
+
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+
+var button = buttons.ActionButton({
+  id: "mozilla-link",
+  label: "Visit Mozilla",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleClick
+});
+
+function handleClick(state) {
+  var currentTab=tabs.activeTab;
+  var url= currentTab.url;
+  url= "weburl://?url="+encodeURI(url);
+  tabs.open({
+    url: url,
+    inBackground: false
+  });
+  tabs.activeTab.close();
+  currentTab.activate();
+}
