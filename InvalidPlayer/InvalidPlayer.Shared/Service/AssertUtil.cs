85799fa26b37d1bde85db5f86ca8918c2b948563
saki <saki@saki.saki>
2015-12-27 00:15:41 +0800

    merge

---
+++ InvalidPlayer/InvalidPlayer.Shared/Service/AssertUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InvalidPlayer.Service
+{
+    public class AssertUtil
+    {
+        public static void IsTrue(bool expression, string message)
+        {
+            if (!expression)
+            {
+                throw new Exception(message);
+            }
+        }
+
+        public static void IsNull(object param, string message)
+        {
+            if (param != null)
+            {
+                throw new Exception(message);
+            }
+        }
+
+        public static void NotNull(object param, string message)
+        {
+            if (param == null)
+            {
+                throw new Exception(message);
+            }
+        }
+
+        public static void HasText(string text, string message)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new Exception(message);
+            }
+        }
+
+        public static void NotEmpty(object[] array, string message)
+        {
+            if (null == array || array.Length < 1)
+            {
+                throw new Exception(message);
+            }
+        }
+    }
+}
