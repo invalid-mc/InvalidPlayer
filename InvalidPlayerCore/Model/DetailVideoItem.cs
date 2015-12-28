45715314eabad53917cd334747db2f37a84a78c8
saki <saki@saki.saki>
2015-12-28 19:09:27 +0800

    support url refresh

---
+++ InvalidPlayerCore/Model/DetailVideoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvalidPlayerCore.Model
+{
+    public class DetailVideoItem:VideoItem
+    {
+        public IEnumerable<KeyValuePair<string, string>> Header { get; set; }
+    }
+}
