90f7d57eee5f3f318d28f88a6f852c67c193dbaf
saki <saki@saki.saki>
2015-12-30 09:28:14 +0800

    Add app service

---
+++ InvalidPlayerCore/Service/AppService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using InvalidPlayerCore.Container;
+
+namespace InvalidPlayerCore.Service
+{
+    [Singleton]
+    public class AppService
+    {
+        private const string RamdomWebUrlApi = "";
+
+        [Inject] private HttpClientService _httpClientService;
+
+        public async Task<string> GetRamdomWebUrl()
+        {
+            var result = await _httpClientService.GetJsonAsync(RamdomWebUrlApi);
+            if (null == result)
+            {
+                return null;
+            }
+            return null;
+        }
+
+        public List<bool> CheckVerison()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetServerVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLocalVersion()
+        {
+            var verson = Package.Current.Id.Version;
+            var num = verson.Build*1000 + verson.Major*100 + verson.Minor*10 + verson.Revision;
+            return num;
+        }
+    }
+}
