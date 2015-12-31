02eb0ea9fdd15bb18eb4514debbf032ae94ac9ab
saki <saki@saki.saki>
2015-12-31 20:45:16 +0800

    add phoe local play support

---
+++ InvalidPlayerCore/Service/RegexHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace InvalidPlayerCore.Service
+{
+    public static class RegexHelper
+    {
+        public static string GetMatchString(string source, Regex regex, int index)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return null;
+            }
+            var match = regex.Match(source);
+            if (match.Success)
+            {
+                return match.Groups[index].ToString();
+            }
+            return null;
+        }
+    }
+}
