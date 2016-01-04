684ff8ed5fcfced78e7a106dcabe6ee1248e1d56
saki <saki@saki.saki>
2016-01-04 16:25:39 +0800

    更新手机布局

--- InvalidPlayer/InvalidPlayer.Shared/View/Player.xaml.cs
+++ InvalidPlayer/InvalidPlayer.Shared/View/Player.xaml.cs
@@ -8,6 +8,7 @@ using Windows.Storage.Pickers;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 using InvalidPlayerCore.Container;
 using InvalidPlayerCore.Model;
@@ -203,5 +204,6 @@ namespace InvalidPlayer.View
                 await Play(file.Path);
             }
         }
+
     }
 }
